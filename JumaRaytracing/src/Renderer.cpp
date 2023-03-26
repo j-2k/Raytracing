af6d0b8d4f7eb020e7dfef31dff9f0c6e8b995d3
Juma <juma_ad@hotmail.com>
2023-03-26 18:32:46 +0400

    Part 3 Completed - Rendering the first Sphere - Implemented the quadratic equation & finding out if the ray hit the sphere with the discriminant

--- JumaRaytracing/src/Renderer.cpp
+++ JumaRaytracing/src/Renderer.cpp
@@ -33,6 +33,10 @@ void Renderer::Render()
 				(float)x / (float)m_FinalImage->GetWidth(),
 				(float)y / (float)m_FinalImage->GetHeight()
 			};
+
+			//remapping coords to -1 to 1 was 0 - 1
+			coord = coord * 2.0f - 1.0f;
+
 			m_ImageData[x + y * m_FinalImage->GetWidth()] = PerPixel(coord);
 
 			//m_ImageData[i] = 0xff00ffff; //ABGR FORMAT (REVERSED RGBA!!!)
@@ -49,8 +53,36 @@ uint32_t Renderer::PerPixel(glm::vec2 fragCoord)
 	uint8_t r = (uint8_t)(fragCoord.x * 255.0f);
 	uint8_t g = (uint8_t)(fragCoord.y * 255.0f);
 
+	//solve for t : t is the magnitude of the ray (quadratic formula below)
+	//  BELOW IS a			BELOW IS b			BELOW IS C
+	// (bx^2 + by^2)t^2 + (2(axbx + ayby))t + (ax^2 + ay^2 - r^2) = 0
+	//x & y is the coords/origin : r is the radius : a is ray origin : b is ray direction
+
+	glm::vec3 rayOrigin(0.f,0.f,2.f);
+	glm::vec3 rayDir(fragCoord.x, fragCoord.y, -1.0f);
+	float radius = 0.5f;
+	//rayDir = glm::normalize(rayDir);
+
+	//final quadratic equation implemented
+	float a = glm::dot(rayDir, rayDir);	//equivalent to float a =	rayDir.x * rayDir.x + rayDir.y * rayDir.y + rayDir.z * rayDir.z;
+	float b = 2.0f * glm::dot(rayOrigin, rayDir);
+	float c = glm::dot(rayOrigin, rayOrigin) - radius * radius;
+
+	//finding out the # of solutions from the quadratic equation
+	//quad formula discriminant = b^2 - 4ac
+	float discriminant = b * b - 4.f * a * c;
+
+	if (discriminant >= 0)
+	{
+		return 0xffff00ff;
+	}
+
+	return 0xff000000;
+
+	/*
 	//this will draw a float4(uv.xy,0,1) quad or "Hello World"
 	return 0xff000000 | (g << 8) | r; //ABGR
 	//BIT SHIFTING RIGHT TO LEFT!!! ABGR FORMAT!
+	*/
 
 }
